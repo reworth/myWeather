522c8c08d971d1c1c298d5ab21557e1daefd707a
reworth <reworth_Yan@163.com>
2016-06-04 20:43:41 +0800

    add the funtion to display the information of weather

--- src/activity/ChooseAreaActivity.java
+++ src/activity/ChooseAreaActivity.java
@@ -3,19 +3,18 @@ package activity;
 import java.util.ArrayList;
 import java.util.List;
 
-import util.HttpCallbackListener;
-import util.HttpUtil;
-import util.Utility;
-
-import com.example.myweather.R;
-
 import model.City;
 import model.County;
 import model.Province;
-
+import util.HttpCallbackListener;
+import util.HttpUtil;
+import util.Utility;
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
@@ -25,6 +24,9 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.myweather.R;
+
 import db.MyWeatherDB;
 
 public class ChooseAreaActivity extends Activity {
@@ -62,12 +64,19 @@ public class ChooseAreaActivity extends Activity {
 	/**
 	 * selectedCounty
 	 */
-	private County selectedCounty;
+	
 	private int currentLevel;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		if(prefs.getBoolean("city_selected", false)){
+			Intent intent = new Intent(this,WeatherActivity.class);
+			startActivity(intent);
+			finish();
+			return;
+		}
 		requestWindowFeature(Window.FEATURE_NO_TITLE);//no title in this page
 		setContentView(R.layout.choose_area);
 		listView = (ListView) findViewById(R.id.list_view);
@@ -86,6 +95,12 @@ public class ChooseAreaActivity extends Activity {
 				}else if(currentLevel == LEVEL_CITY){
 					selectedCity = cityList.get(index);
 					queryCounties();
+				}else if(currentLevel == LEVEL_COUNTY){
+					String countyCode = countyList.get(index).getCountyCode();
+					Intent intent = new Intent(ChooseAreaActivity.this,WeatherActivity.class);
+					intent.putExtra("county_code", countyCode);
+					startActivity(intent);
+					finish();
 				}
 			}
 		});
@@ -120,7 +135,7 @@ public class ChooseAreaActivity extends Activity {
 			titleText.setText(selectedCity.getCityName());
 			currentLevel = LEVEL_COUNTY;
 		}else{
-			qureyFromServer(null,"province");
+			qureyFromServer(selectedCity.getCityCode(),"county");
 		}
 	}
 
